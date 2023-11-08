f0fbc96c468f91edb6ae9bed4375301e8e0473e3
Cody <codylau6233@hotmail.com>
2023-11-07 21:35:32 -0800

    Add constants.js file: move constants IP, PORT and Movement Key to constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = 'localhost';
+const PORT = 50541;
+const playerName = "Name: CWL";
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 'd';
+
+
+module.exports = {
+  IP,
+  PORT,
+  playerName,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY
+};
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 // Stores the active TCP connection object.
+const {MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY} = require('./constants');
 let connection;
 
 // Update the setupInput() function to accept a single parameter and assign its value to the connection variable.
@@ -17,16 +18,16 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
-  if (data === 'w') {
+  if (data === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (data === 'a') {
+  if (data === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (data === 's') {
+  if (data === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (data === 'd') {
+  if (data === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
   if (data === 'k') {
