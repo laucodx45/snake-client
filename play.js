640507936b45222ec20508958351378f9005aa2b
Cody <codylau6233@hotmail.com>
2023-11-07 17:10:22 -0800

    Add comments in play.js: -Comments in play.js are codes that test new communication protocols 'Move: up'

--- play.js
+++ play.js
@@ -12,13 +12,29 @@ const connect = () => {
 
   // when client is connected to server
   conn.on("connect", () => {
+
     console.log("Successfully connected to game server");
+
+    // send server our name
+    conn.write("Name: CWL");
+    // conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 100);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 150);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 200);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 250);
   });
 
-  conn.on("connect", () => {
-    conn.write("Name: CWL");
-  });
-  
   conn.on("data", (data) => {
     console.log(data);
   });
