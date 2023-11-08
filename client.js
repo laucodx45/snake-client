091a7d29dbb4f61dc7802ab93b805b9ce38f021b
Cody <codylau6233@hotmail.com>
2023-11-07 20:17:00 -0800

    Refactor play.js: Move connect function to client.js and setupInput function to new file input.js

--- client.js
+++ client.js
@@ -1,1 +1,30 @@
-const myObject = require('./play');
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // when client is connected to server
+  conn.on("connect", () => {
+
+    console.log("Successfully connected to game server");
+    
+    conn.write("Name: CWL");
+    
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,65 +1,8 @@
-const net = require("net");
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-// establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // when client is connected to server
-  conn.on("connect", () => {
-
-    console.log("Successfully connected to game server");
-
-    // send server our name
-    conn.write("Name: CWL");
-    // conn.write("Move: up");
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 50);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 100);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 150);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 200);
-    // setTimeout(() => {
-    //   conn.write("Move: up");
-    // }, 250);
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  // event listener for stdin
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
-module.exports = {
-  connect
-};
