2188075a2e92b5aa2026f87993977723bc239122
Cody <codylau6233@hotmail.com>
2023-11-14 15:25:41 -0800

    Refactor client.js

--- client.js
+++ client.js
@@ -9,21 +9,19 @@ const connect = () => {
     port: PORT
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   // when client is connected to server
   conn.on("connect", () => {
-
     console.log("Successfully connected to game server");
-    
     conn.write(playerName);
-    
   });
 
+  // event listener, prints data sent from server to client
   conn.on("data", (data) => {
     console.log(data);
   });
+
   return conn;
 };
 
