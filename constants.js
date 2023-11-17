1ffd16d6a6820765f0c606c13a68be6bcffe4fbb
Cody <codylau6233@hotmail.com>
2023-11-16 21:36:12 -0800

    Add new movementObj to simply code in constant.js

--- constants.js
+++ constants.js
@@ -1,10 +1,12 @@
 const IP = 'localhost';
 const PORT = 50541;
 const playerName = "Name: CWL";
-const MOVE_UP_KEY = 'w';
-const MOVE_DOWN_KEY = 's';
-const MOVE_LEFT_KEY = 'a';
-const MOVE_RIGHT_KEY = 'd';
+const movementObj = {
+  MOVE_UP_KEY : 'w',
+  MOVE_DOWN_KEY : 's',
+  MOVE_LEFT_KEY : 'a',
+  MOVE_RIGHT_KEY : 'd'
+};
 const messageObj = {
   k: "Say: ME AS A BABY",
   l: "Say: I DREAM RECURSION"
@@ -14,9 +16,6 @@ module.exports = {
   IP,
   PORT,
   playerName,
-  MOVE_UP_KEY,
-  MOVE_DOWN_KEY,
-  MOVE_LEFT_KEY,
-  MOVE_RIGHT_KEY,
+  movementObj,
   messageObj
 };
